45c5e7f5919bb4ddcd33279a85c418ef22f91390
franspaco <franspaco@gmail.com>
2018-04-21 17:28:57 -0500

    Inserted queue into project

---
+++ queue/queue.c
@@ -0,0 +1,77 @@
+/*
+ * Implementation of thread-safe queue. It supports push and pop
+ * operations
+ */
+
+#include "queue.h"
+
+/*
+ * Initializes the queue and its mutex
+ *
+ */
+QueueHeader* createQueue() {
+  struct QueueHeader *handle = malloc(sizeof(*handle));
+  handle->head = NULL;
+  handle->tail = NULL;
+
+  pthread_mutex_init(&(handle->mutex), NULL);
+  
+  return handle;
+}
+
+/*
+ * Destroys the queue pointer.
+ */
+void destroy(struct QueueHeader *header) {
+  free(header);
+  header = NULL;
+}
+
+/*
+ * Pushes an element to the queue in a thread-safe way.
+ */
+void push(struct QueueHeader *header, void *elem) {
+  // Create new element
+  QueueElem *element = malloc(sizeof(*element));
+  element->value = elem;
+  element->next = NULL;
+
+  pthread_mutex_lock(&(header->mutex));
+
+  // Checks if list is empty
+  if (header->head == NULL) {
+  	header->head = element;
+  	header->tail = element;
+  } else {
+  	// Change the tail
+  	struct QueueElem* oldTail = header->tail;
+  	oldTail->next = element;
+  	header->tail = element;
+  }
+  pthread_mutex_unlock(&(header->mutex));
+}
+
+/*
+ * Dequeues an element from the queue
+ */
+void* pop(struct QueueHeader *header) {
+  pthread_mutex_lock(&(header->mutex));
+  QueueElem *head = header->head;
+
+  // Checks if list is empty
+  if (head == NULL) {
+  	pthread_mutex_unlock(&(header->mutex));
+  	return NULL;
+  } else {
+	  // Change the head
+  	header->head = head->next;
+  	
+  	// Get head and free element memory
+  	void *value = head->value;
+  	free(head);
+  	
+  	pthread_mutex_unlock(&(header->mutex));
+  	return value;
+  }
+}
+
