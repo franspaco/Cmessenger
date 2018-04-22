b9d5938081600a90f8d259982d3f75e901450a88
franspaco <franspaco@gmail.com>
2018-04-21 23:04:40 -0500

    Heavy duty tester

--- msgr_test_client.c
+++ msgr_test_client.c
@@ -18,6 +18,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <time.h>
 // Sockets libraries
 #include <netdb.h>
 #include <arpa/inet.h>
@@ -26,40 +28,53 @@
 // Custom libraries
 #include "sockets.h"
 #include "fatal_error.h"
+#include "protocol.h"
 
 #define BUFFER_SIZE 1024
 
+#define THREAD_POOL 5
+#define MSG_COUNT 5
+
 ///// FUNCTION DECLARATIONS
 void usage(char * program);
-void client_ops(int fd);
+void* threads(void* arg);
+
 
 void purgeStdin(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
+typedef struct thread_data {
+    int id;
+    char* host;
+    char* port;
+    char* dest;
+} tdata_t;
+
 ///// MAIN FUNCTION
-int main(int argc, char * argv[])
-{
-    int connection_fd;
-
-    printf("\n=== BANK CLIENT PROGRAM ===\n");
-
+int main(int argc, char * argv[]) {
+    srand(time(NULL));
     // Check the correct arguments
-    if (argc != 3)
-    {
+    if (argc != 4) {
         usage(argv[0]);
     }
 
-    // Start the server
-    connection_fd = connectSocket(argv[1], argv[2]);
-	
+    pthread_t tids[THREAD_POOL];
 
-    client_ops(connection_fd);
-    
+    for(int i = 0; i < THREAD_POOL; i++){
+        tdata_t* data = malloc(sizeof(tdata_t));
+        data->id = i+1;
+        data->host = argv[1];
+        data->port = argv[2];
+        data->dest = argv[3];
+        pthread_create(&tids[i], NULL, threads, (void*)data);
+        printf("[%i] CREATED.\n", i+1);
+    }
 
-    // Close the socket
-    close(connection_fd);
+    for(int i = 0; i < THREAD_POOL; i++){
+        pthread_join(tids[i], NULL);
+    }
 
     return 0;
 }
@@ -70,69 +85,77 @@ int main(int argc, char * argv[])
 void usage(char * program)
 {
     printf("Usage:\n");
-    printf("\t%s {server_address} {port_number}\n", program);
+    printf("\t%s {server_address} {port_number} {target username}\n", program);
     exit(EXIT_FAILURE);
 }
 
-void client_ops(int fd){
-    char buffer[1024];
+char randChar(){
+    return rand()%26 + 97;
+}
 
-    // Stuff required by poll stdin
-    struct pollfd test_fds[1];
-    int timeout = 10; // 10ms tiemout
-    int poll_result;
-
-    char input = 0;
-    while(input != 'x'){
-        printf("OPTs:\n s - send\n x - exit\n");
-        while(1){
-            test_fds[0].fd = stdin->_fileno;
-            test_fds[0].events = POLLIN;
-            poll_result = poll(test_fds, 1, timeout);
-            if(poll_result == -1){
-                //??
-                fatalError("STDIN poll");
-            }
-            else if(poll_result == 0){
-                // Nothing: check for server replies
-                test_fds[0].fd = fd;
-                test_fds[0].events = POLLIN;
-                poll_result = poll(test_fds, 1, timeout);
-                if(poll_result == -1){
-                    //??
-                    fatalError("Server poll");
-                }
-                else if(poll_result == 0){
-                    // Nothing: continue polling
-                    continue;
-                }
-                else{
-                    // Got something, most likely a BYE: read and make sure
-                    // Receive the response
-                    if (recvString(fd, buffer, BUFFER_SIZE) ){
-                        printf("\n\nServer sent: %s\n", buffer);
-                        //exit(0);
-                    }
-                    else {
-                        exit(0);
-                    }
-                }
-            }
-            else{
-                scanf(" %c", &input);
-                break;
-            }
-        }
-
-        switch(input){
-            case 'x':
-                exit(0);
-            case 's':
-                printf("Write:\n");
-                purgeStdin();
-                fgets(buffer, 1024, stdin);
-                printf("sending: %s\n", buffer);
-                sendString(fd, buffer);
-        }
+void randStr(char* str, int size){
+    for(int i = 0; i < size-1; i++){
+        str[i] = randChar();
     }
+    str[size-1] = '\0';
+}
+
+void threadCleanup(tdata_t* data, int fd){
+    printf("[%i] DESTROYED.\n", data->id);
+    // Close the socket
+    close(fd);
+    // Free data struct pased on to the thread
+    free(data);
+    pthread_exit(NULL);
+}
+
+void* threads(void* arg){
+    //Set up
+    long id;
+    packet_t packet;
+    tdata_t* data = (tdata_t*)arg;
+    char uname[UNAME_LENGTH];
+    sprintf(uname, "thread%02i", data->id);
+
+    // Start conenction
+    int fd = connectSocket(data->host, data->port);
+
+    // Send start with username
+    sendCodeStr(fd, C_START, uname);
+    if(!readPacket(fd, &packet) && packet.code != REQ_OK){
+        // error
+        threadCleanup(data, fd);
+    }
+
+    sendCodeStr(fd, QRY_USR, data->dest);
+    if(!readPacket(fd, &packet) && packet.code != USR_FND){
+        // error
+        threadCleanup(data, fd);
+    }
+    id = packet.id;
+
+    usleep(100000*(rand()%10+1));
+    
+    sendCodeIdStr(fd, SND_MSG, id, "HI!");
+    printf("[%i] Hello string -> %li\n", data->id, id);
+    if(!readPacket(fd, &packet)){
+        threadCleanup(data, fd);
+    }
+
+    usleep(100000*(rand()%10+1));
+
+    char message[30];
+    for(int i = 0; i < MSG_COUNT; i++){
+        randStr(message, 30);
+        sendCodeIdStr(fd, SND_MSG, id, message);
+        printf("[%i] %s -> %li\n", data->id, message, id);
+        if(!readPacket(fd, &packet)){
+            threadCleanup(data, fd);
+        }
+        usleep(100000*(rand()%10+1));
+    }
+
+    sendCode(fd, C_QUIT);
+
+    threadCleanup(data, fd);
 }
