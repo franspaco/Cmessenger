b9537ff6974953e18fb47539fdb3facc8e5e1931
franspaco <franspaco@gmail.com>
2018-04-21 23:51:26 -0500

    testing workloads

--- msgr_test_listener.c
+++ msgr_test_listener.c
@@ -5,15 +5,6 @@
  * A01019512
  */
 
-/*
-    Client program to access the accounts in the bank
-    This program connects to the server using sockets
-
-    Gilberto Echeverria
-    gilecheverria@yahoo.com
-    29/03/2018
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
